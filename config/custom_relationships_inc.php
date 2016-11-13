0102a3cb914eca21f1077fc0e9dd7764b80f9d42
infojunkie <karim.ratib@gmail.com>
2016-11-13 14:54:06 -0800

    Add relationship definitions

--- config/custom_relationships_inc.php
+++ config/custom_relationships_inc.php
@@ -1,4 +1,9 @@
 <?php
+
+  // custom relationships
+  define( 'BUG_CUSTOM_RELATIONSHIP_ORIGIN_OF',       99 );
+  define( 'BUG_CUSTOM_RELATIONSHIP_ORIGINATES_FROM', 98 );
+
   $g_relationships[ BUG_CUSTOM_RELATIONSHIP_ORIGIN_OF ] = array(
     '#forward' => true,
     '#complementary' => BUG_CUSTOM_RELATIONSHIP_ORIGINATES_FROM,
@@ -7,7 +12,7 @@
     '#notify_deleted' => 'email_notification_title_for_action_origin_of_relationship_deleted',
     '#edge_style' => array ( 'style' => 'dashed', 'color' => '808080' ),
   );
- 
+
   $g_relationships[ BUG_CUSTOM_RELATIONSHIP_ORIGINATES_FROM ] = array(
     '#forward' => false,
     '#complementary' => BUG_CUSTOM_RELATIONSHIP_ORIGIN_OF,
